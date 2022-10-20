02d66595b8699b666bdc5418e0b213f2f8af85b1
munir <munirumujuzi@gmail.com>
2022-10-20 02:26:03 -0700

    task o

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,23 @@
 #include "main.h"
+
 /**
- * main - print _putchar
- * is written by Munir
- * Return: 0
- */
+*main - Entry point
+*
+*Return: 0 nothing, return void
+*/
 
 int main(void)
+
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	_putchar('__');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,6 @@
-#if ndef MAIN_H
+#if nder MAIN_H
 #define MAIN_H
- int _putchar{char c}
+
+int _putchar(char c)
+
 #endif
